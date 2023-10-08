14f7084ea12d2dddbbb005764ed191910c8588af
Jonilson Martins <141881300+jonilsonmartins@users.noreply.github.com>
2023-10-07 22:49:32 -0300

    melhora da tela de cadastro, inserção do logo

--- App.js
+++ App.js
@@ -3,13 +3,17 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/pages/Login/';
 import Cadastro from './src/pages/Cadastro/';
+import {useFonts} from 'expo-font';
 
+const navigationOptions = {
+  headerShown: false,
+}
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="Login" screenOptions={navigationOptions}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
       </Stack.Navigator>
