609352ef5209d43fb79afdc784cc5c124b6889cf
Jonilson Martins <141881300+jonilsonmartins@users.noreply.github.com>
2023-10-14 18:41:18 -0300

    criação da home

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Login from './src/pages/Login/';
 import Cadastro from './src/pages/Cadastro/';
+import Home from './src/pages/Home/';
 import {useFonts} from 'expo-font';
 
 const navigationOptions = {
@@ -13,9 +14,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login" screenOptions={navigationOptions}>
+      <Stack.Navigator initialRouteName="Home" screenOptions={navigationOptions}>
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Cadastro" component={Cadastro} />
+        <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
   );
